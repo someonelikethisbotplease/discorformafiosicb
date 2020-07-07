32183d8375aa4e27e7b1a5d366ec35b7b49c9050
someonelikethisbotplease <67959753+someonelikethisbotplease@users.noreply.github.com>
2020-07-07 19:16:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ Bot = commands.Bot(command_prefix= '!')
 async def on_ready():
 
 	print("Bot is online!")
-	
+Bot.remove_command('help')	
 @Bot.command(pass_context=True)
 async def hello(ctx):
 	if ctx.author.id == 603860059595866115:
@@ -43,7 +43,33 @@ async def WorldWarPolygonScript(ctx):
 async def ChickenGunScript(ctx):
 	await ctx.send("https://shre.su/55P1")
 
+@Bot.command(pass_context=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+	if ctx.author.id in [603860059595866115,410404778678681610,620324517990105108,543108160748257304,712365589271674894,635484724718206976,506886660567334934]:
+		await member.ban(reason=reason)
+		await ctx.send('User {0} banned! '.format(member) + 'Причина : {0}'.format(reason))
+	else:
+		await ctx.send('Ты не пройдёёшь!')
+@Bot.command(pass_context=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+	if ctx.author.id in [603860059595866115,410404778678681610,620324517990105108,543108160748257304,712365589271674894,635484724718206976,506886660567334934]:
+		await member.kick(reason=reason)
+		await ctx.send('User {0} kicked! '.format(member) + 'Причина : {0}'.format(reason))   
+	else:
+		await ctx.send('Ты не пройдёёшь!')		
 
+@Bot.command(pass_context=True)
+async def help(ctx):
+	emb = discord.Embed(title="Информация о командах.", colour = 0x39d0d6)
+	emb.add_field(name = '{}help'.format(prefix), value="Выводит информацию о командах.")
+	emb.add_field(name = '{}hello'.format(prefix), value="Тестовая функция приветствия.")
+	emb.add_field(name = '{}MadGunZ_Script'.format(prefix), value="Скрипт на Mad GunZ(free).")
+	emb.add_field(name = '{}BlockManGoScript'.format(prefix), value="Скрипт на BlockMan GO(free).")
+	emb.add_field(name = '{}WorldWarPolygonScript'.format(prefix), value="Скрипт на World War Polygon(free).")
+	emb.add_field(name = '{}ChickenGunScript'.format(prefix), value="Скрипт на Chicken Gun(free).")
+	emb.add_field(name = '{}ban'.format(prefix), value="С этой функцией можно забанить человека.")
+	emb.add_field(name = '{}kick'.format(prefix), value="С этой функцией можно кикнуть человека.")
+	await ctx.send(embed=emb)
 
 @Bot.event
 async def on_member_join(member):
